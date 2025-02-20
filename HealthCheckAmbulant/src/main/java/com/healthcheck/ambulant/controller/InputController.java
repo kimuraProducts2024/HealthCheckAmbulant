d3f095d6f349a486113c68f889fcaf0f41fb6132
kimuraProducts2024 <kimura_0222taku@outlook.jp>
2025-02-20 22:42:03 +0900

    modified all file

--- HealthCheckAmbulant/src/main/java/com/healthcheck/ambulant/controller/InputController.java
+++ HealthCheckAmbulant/src/main/java/com/healthcheck/ambulant/controller/InputController.java
@@ -6,25 +6,27 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.healthcheck.ambulant.common.CommonFunc;
-import com.healthcheck.ambulant.entity.M_Test_Item;
+import com.healthcheck.ambulant.entity.MTestItem;
 import com.healthcheck.ambulant.form.InputForm;
-import com.healthcheck.ambulant.form.InputForm.InputType;
-import com.healthcheck.ambulant.service.M_Test_ItemService;
+import com.healthcheck.ambulant.service.MTestItemService;
 
 import jakarta.servlet.http.HttpSession;
 
 /**
  * 入力処理コントローラ
- * 確認画面、登録画面の処理を行う
+ * 確認画面、登録画面への遷移や処理を行う
  */
 @Controller
 public class InputController {
 	// 検査項目情報
-	M_Test_Item mTestItem;
+	MTestItem mTestItem;
 	
 	// ユーザ検査項目サービス定義
 	@Autowired
-	M_Test_ItemService mTestItemService;
+	MTestItemService mTestItemService;
+	
+	// 共通機能クラス変数
+	CommonFunc comFunc = new CommonFunc();
 	
 	/**
 	 * 入力画面：戻るボタンクリック
@@ -42,7 +44,7 @@ public class InputController {
 	}
 	
 	/**
-	 * 確認ボタンクリック
+	 * 各入力画面：確認ボタンクリック
 	 * @param inputForm 入力画面情報
 	 * @param session セッション
 	 * @param model モデル
@@ -51,44 +53,8 @@ public class InputController {
 	@PostMapping(value = "/input", params = "confirmBtn")
 	public String showConfirm(InputForm inputForm, HttpSession session, Model model)
 	{
-		// 画面が身長入力画面の場合
-		if (session.getAttribute("InputType").equals(InputType.HEIGHT)) {
-			// 身長整数部が未入力の場合
-			if (inputForm.getIntegerPart() == null) {
-				// エラーメッセージの設定
-				inputForm.setErrorLabel("身長整数部の値を入力してください。");
-				
-				// Modelに登録
-				model.addAttribute("inputForm", inputForm);
-				
-				// 遷移先のHTMLファイル名を返す
-				return "InputHeight";
-			}
-			
-			// 身長小数部が未入力の場合
-			if (inputForm.getDecimalPart() == null) {
-				// エラーメッセージの設定
-				inputForm.setErrorLabel("身長小数部の値を入力してください。");
-				
-				// Modelに登録
-				model.addAttribute("inputForm", inputForm);
-				
-				// 遷移先のHTMLファイル名を返す
-				return "InputHeight";
-			}
-			
-			// 画面表示文字列を身長に設定
-			inputForm.setLabelPart("身長");
-			
-			// InputTypeをHEIGHTに設定
-			inputForm.setInputType(InputType.HEIGHT);
-		}
-		
-		// Modelに登録
-		model.addAttribute("inputForm", inputForm);
-		
-		// 遷移先のHTMLファイル名を返す
-		return "Confirm";
+		// 入力値チェックを行い、次画面への遷移先を返す
+		return comFunc.getNextConfPageName(inputForm, session, model);
 	}
 	
 	/**
@@ -101,27 +67,13 @@ public class InputController {
 	@PostMapping(value = "/confirm", params = "returnBtn")
 	public String returnInput(InputForm inputForm, HttpSession session, Model model)
 	{
-		// 入力タイプの取得
-		InputType inputType = (InputType)session.getAttribute("InputType");
-		
-		// メッセージの初期設定
-		inputForm.setErrorLabel("");
-		
-		// 画面が身長入力画面の場合
-		if (inputType.equals(InputType.HEIGHT)) {
-			// Modelに登録
-			model.addAttribute("inputForm", inputForm);
-			
-			// 遷移先のHTMLファイル名を返す
-			return "InputHeight";
-		}
-		
-		return "redirect:/";
+		// 入力画面情報を元に、入力画面へ戻る
+		return comFunc.getPrevInputPageName(inputForm, session, model);
 	}
 	
 	/**
 	 * 確認画面：確定ボタンクリック
-	 * 入力値から、M_Test_Itemテーブルの情報を更新し、
+	 * 入力値から、MTestItemテーブルの情報を更新し、
 	 * 完了の場合、登録完了画面へ遷移する
 	 * @param inputForm 入力画面情報
 	 * @param session セッション
@@ -131,31 +83,7 @@ public class InputController {
 	@PostMapping(value = "/confirm", params = "completeBtn")
 	public String showComplete(InputForm inputForm, HttpSession session, Model model)
 	{
-		// 入力タイプの取得
-		InputType inputType = (InputType)session.getAttribute("InputType");
-		
-		// 身長入力の場合
-		if (inputType.equals(InputType.HEIGHT)) {
-			// 共通機能クラス変数
-			CommonFunc comFunc = new CommonFunc();
-			 
-			// ユーザIDから対象検査項目情報を更新
-			int resultInt = comFunc.updateHeight(session, mTestItemService, inputForm.getIntegerPart(), inputForm.getDecimalPart());
-			
-			// 更新件数が0件以下の場合
-			if (resultInt <= 0) {
-				// エラーメッセージの設定
-				inputForm.setErrorLabel("データの更新に失敗しました。");
-				
-				// リダイレクト変数の設定
-				model.addAttribute("inputType", inputType);
-				
-				// 確認画面に戻る
-				return "InputHeight";
-			}
-		}
-		
-		// 遷移先のHTMLファイル名を返す
-		return "Complete";
+		// ユーザIDを元に、対象検査項目情報の更新処理を実施
+		return comFunc.updateMTestItem(inputForm, session, model, mTestItem, mTestItemService);
 	}
 }
